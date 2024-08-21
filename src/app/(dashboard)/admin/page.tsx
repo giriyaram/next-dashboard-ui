d317a21f2ff32b71f0fa7963a512eafbb22e1a24
Giri Yaram <yaramgiri@gmail.com>
2024-08-21 11:59:53 +0530

    added dashboard layout

---
+++ src/app/(dashboard)/admin/page.tsx
@@ -0,0 +1,40 @@
+import AttendenceChart from "@/app/components/AttendenceChart";
+import CountChart from "@/app/components/CountChart";
+import UserCard from "@/app/components/UserCard";
+import React from "react";
+
+const AdminPage = () => {
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* LEFT */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
+        {/* user Cards */}
+        <div className="flex gap-4 justify-between flex-wrap">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parent" />
+          <UserCard type="staff" />
+        </div>
+
+        {/* MIDDLE CHARTS */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* COUNT CHART */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* ATTENDANCE CHART */}
+          <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendenceChart />
+          </div>
+        </div>
+        {/* BOTTOM CHARTS  */}
+        <div></div>
+      </div>
+
+      {/* RIGHT */}
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
+  );
+};
+
+export default AdminPage;

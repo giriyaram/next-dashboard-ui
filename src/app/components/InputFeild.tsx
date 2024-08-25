5a4abbb920cfce6753f0088f6479d35079fe2e19
Giri Yaram <yaramgiri@gmail.com>
2024-08-25 22:55:55 +0530

    created a Input feild component for the forms

---
+++ src/app/components/InputFeild.tsx
@@ -0,0 +1,42 @@
+import { register } from "module"
+import { FieldError } from "react-hook-form";
+
+type InputFeildProps = {
+    label : string;
+    type?: string;
+    register: any;
+    name: string;
+    defaultValue?: string;
+    error?: FieldError;
+    inputProps?: React.InputHTMLAttributes<HTMLAllCollection>;
+}
+
+const InputFeild = ({
+    label,
+    type = "text",
+    register,
+    name,
+    defaultValue,
+    error,
+    inputProps
+} : InputFeildProps ) => {
+  return (
+    <div className="flex flex-col gap-2 w-full md:w-1/4">
+    <label className="text-xs text-gray-500">{label}</label>
+    <input
+      type={type}
+      {...register(name)}
+      className="ring-[1.5px] ring-gray-300 p-2 rounded-md text-sm w-full "
+      {...inputProps}
+      defaultValue={defaultValue}
+    />
+    {error?.message && (
+      <p className="text-xs text-red-400">
+        {error?.message.toString()}
+      </p>
+    )}
+    </div>
+  )
+}
+
+export default InputFeild

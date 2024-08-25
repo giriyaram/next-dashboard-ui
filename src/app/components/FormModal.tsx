d28aec7c77d5883791920059ad997fd797390a08
Giri Yaram <yaramgiri@gmail.com>
2024-08-25 16:27:28 +0530

    created form modal component

---
+++ src/app/components/FormModal.tsx
@@ -0,0 +1,56 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import { useState } from "react";
+
+// USE LAZY LOADING
+
+// import TeacherForm from "./forms/TeacherForm";
+// import StudentForm from "./forms/StudentForm";
+
+
+
+const FormModal = ({
+  table,
+  type,
+  data,
+  id,
+}: {
+  table:
+    | "teacher"
+    | "student"
+    | "parent"
+    | "subject"
+    | "class"
+    | "lesson"
+    | "exam"
+    | "assignment"
+    | "result"
+    | "attendance"
+    | "event"
+    | "announcement";
+  type: "create" | "update" | "delete";
+  data?: any;
+  id?: number;
+}) => {
+  const size = type === "create" ? "w-8 h-8" : "w-7 h-7";
+  const bgColor =
+    type === "create"
+      ? "bg-lamaYellow"
+      : type === "update"
+      ? "bg-lamaSky"
+      : "bg-lamaPurple";
+
+  
+
+  return (
+    <>
+      <button className={`${size} flex items-center justify-center rounded-full ${bgColor} `} >
+        <Image src={`/${type}.png`} alt="" width={16} height={16 } />
+      </button>
+    </>
+  );
+};
+
+export default FormModal;

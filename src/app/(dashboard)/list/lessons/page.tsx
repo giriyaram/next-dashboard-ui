39c87ab73dfe6bab68ce3330aa71f4a49c6b9592
Giri Yaram <yaramgiri@gmail.com>
2024-09-09 20:51:34 +0530

    added admin page as home page

--- src/app/(dashboard)/list/lessons/page.tsx
+++ src/app/(dashboard)/list/lessons/page.tsx
@@ -44,7 +44,7 @@ const LessonListPage = () => {
       <td className="hidden md:table-cell">{item.teacher}</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/teachers/${item.id}`}>
+          <Link href={`/list/lessons/${item.id}`}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
               <Image src="/edit.png" alt="" width={16} height={16} />
             </button>
